1ca79d9e3e60afc5f8bc775d63f18202f58a6b2a
System0010 <emilio.arevalozuniga@gmail.com>
2023-05-19 18:34:15 +0200

    web page klutz

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+  distance: '65px',
+  duration: 2600,
+  delay: 450,
+  reset: true
+});
+
+sr.reveal('.klutzyboy-text',{delay:200, origin:'top'});
