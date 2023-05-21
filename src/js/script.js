6acc0ff4e8168d594c39bcbaf17d07a444c006eb
System0010 <emilio.arevalozuniga@gmail.com>
2023-05-21 13:28:45 +0200

    adding footer

--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,24 @@ menu.onclick = () =>{
   menu.classList.toggle('bx-x');
   navlist.classList.toggle('open');
 };
-
+//scroll instance
 const sr = ScrollReveal ({
   distance: '65px',
   duration: 2600,
   delay: 450,
   reset: true
 });
-
+//apply scroll for .klutzyboy-text
 sr.reveal('.klutzyboy-text',{delay:200, origin:'top'});
+
+//footer js hidden show 
+let scrollDown = document.querySelector('.scroll-down');
+let footer = document.querySelector('footer');
+let fnav = document.querySelector('.footer-navlist');
+
+scrollDown.onclick = () =>{
+footer.style.display = "block";
+fnav.style.display = "flex";
+}
+
+//.klutzyboy-img js code to scroll origin right
