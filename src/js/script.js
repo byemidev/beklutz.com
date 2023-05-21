07b53218800a3ceece05cd33f4b094cee1cbec23
System0010 <emilio.arevalozuniga@gmail.com>
2023-05-21 17:37:33 +0200

    klutzboy-img fixed, icons included in 680 and lower resol testing on iphone 7plus resol

--- src/js/script.js
+++ src/js/script.js
@@ -13,14 +13,16 @@ const sr = ScrollReveal ({
   reset: true
 });
 //apply scroll for .klutzyboy-text
-sr.reveal('.klutzyboy-text',{delay:210, origin:'top'});
+sr.reveal('.klutzyboy-text',{delay:200, origin:'top'});
 
 //footer js hidden show 
 let menuFooter = document.querySelector('#footer-icon');
 let footer = document.querySelector('footer');
+let fnav = document.querySelector('.footer-navlist');
 
 menuFooter.onclick = () =>{
 footer.classList.toggle('open');
+sr.reveal(fnav,{delay:0, origin:'left'});
 }
 
 //.klutzyboy-img js code to scroll origin right
